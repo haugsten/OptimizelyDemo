ad5b2f16494e38e1f02b046a265ead0ca95f9a60
Haakon Peder Haugsten <haugsten@users.noreply.github.com>
2021-11-24 13:05:12 +0100

    Feature/cms (#2)
    * Added ImageFile
    
    * Added image to TeaserBlock view
    
    * Added App_Data to .gitignore

---
+++ EmptySite/Models/Media/ImageFile.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.DataAnnotations;
+
+namespace EmptySite.Models.Media
+{
+    [ContentType(GUID = "c9f15c66-5baf-4631-b7e9-e69557e3cc35")]
+    [MediaDescriptor(ExtensionString = "jpg,jpeg,jpe,ico,gif,bmp,png")]
+    public class ImageFile : ImageData
+    {
+        /// <summary>
+        /// Gets or sets the copyright.
+        /// </summary>
+        /// <value>
+        /// The copyright.
+        /// </value>
+        public virtual string Copyright { get; set; }
+    }
+}

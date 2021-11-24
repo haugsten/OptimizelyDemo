99147eb903cf30e2e3f3ef481084dff9a230783e
Haakon Haugsten <haakon@episerver.no>
2021-11-24 13:44:57 +0100

    Added controller and view for GenericProduct

---
+++ EmptySite/Controllers/GenericProductController.cs
@@ -0,0 +1,15 @@
+﻿using EmptySite.Models.Catalog;
+using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmptySite.Controllers
+{
+    public class GenericProductController : ContentController<GenericProduct>
+    {
+
+        public ActionResult Index(GenericProduct currentContent)
+        {
+            return View(currentContent);
+        }
+    }
+}
---
+++ EmptySite/Extensions/CommerceMediaExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using EPiServer.Commerce.Catalog.ContentTypes;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiServer.Web;
+
+namespace EmptySite.Extensions
+{
+    public static class CommerceMediaExtensions
+    {
+
+        public static ContentReference AssetImageUrl(this EntryContentBase entry)
+        {
+            if (entry != null)
+            {
+                if (entry.CommerceMediaCollection != null)
+                {
+                    foreach (var commerceMedia in entry.CommerceMediaCollection)
+                    {
+                       
+                            return commerceMedia.AssetLink;
+                    }
+
+                }
+            }
+            return ContentReference.EmptyReference;
+        }
+    }
+}
